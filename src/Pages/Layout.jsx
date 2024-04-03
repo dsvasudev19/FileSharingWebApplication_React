c62b7e9e7b95f0608e9801dbd80df89f136cff3f
dsvasudev <vasudevds1729@gmail.com>
2024-04-03 10:56:28 +0530

    updated styles

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -3,15 +3,16 @@ import NavigationMenu from "../Components/NavigationMenu";
 import {Outlet} from "react-router-dom";
 import SideBar from "../Components/SideBar";
 import Footer from "../Components/Footer";
+import './../App.css'
 export default function Layout() {
     return (
-        <div className="align-items-center justify-items-center">
+        <div className="align-items-center justify-items-center max-h-100">
             <NavigationMenu className="sticky top-0"/>
             <div className='flex flex-row '>
                 <div id='sideBar' className='w-[16%] md:w-[18%] rounded-lg bg-white mt-5 overflow-hidden'>
                     <SideBar className="bg-white"/>
                 </div>
-                <div id='mainContent' className='w-[82%] rounded-lg ml-2'>
+                <div id='mainContent' className='w-[82%] rounded-lg ml-2 max-h-[90vh] overflow-y-auto'>
 
                     <Outlet />
                 </div>
