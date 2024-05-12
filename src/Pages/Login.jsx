e96b09ff4d10ba56d1b69f41c238ca0e44c6e766
dsvasudev <vasudevds1729@gmail.com>
2024-05-12 09:06:11 +0530

    Added download feature

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -31,6 +31,8 @@ const Login = ({ className }) => {
     }
   };
 
+  
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
